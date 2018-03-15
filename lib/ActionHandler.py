70648ef0ce04cdf5e0f7766cdb7a8ffb4a88d3a8
Fabio Viola <desmovalvo@gmail.com>
2018-03-15 13:50:48 +0100

    Added n3 output of the request and reading of the parameters

--- lib/ActionHandler.py
+++ lib/ActionHandler.py
@@ -5,6 +5,10 @@ import vamp
 import numpy
 import logging
 import librosa
+from rdflib import *
+
+# local reqs
+from lib.utils import *
 
 class ActionHandler:
 
@@ -19,6 +23,7 @@ class ActionHandler:
         logging.debug("ActionHandler::__init__() invoked")
 
         # store the kp and the jsap object
+        self.n3File = "transform.n3"
         self.jsap = jsap
         self.kp = kp
         
@@ -40,40 +45,48 @@ class ActionHandler:
         actionInstance = None
         actionName = None
         actionUri = None
+        transformUri = None
         songName = "/home/val/QMUL/code/songRepo/audio/Rein_-_Occidente.mp3"        
         
         # iterate over added building
         # TODO -- consider that we may receive multiple action requests with a notification
         #         but for the demo purposes it is not mandatory        
         for result in added:
-
-            # get the action name
             actionURI = result["action"]["value"]
             actionName = result["actionName"]["value"]
             actionInstance = result["instance"]["value"]
+            if result["fieldName"]["value"] == "transformUri":
+                transformUri = result["fieldValue"]["value"]
 
-            # get the parameters
-            # TODO -- for demo purposes we will use default values and a default song
+        # get the transform!
+        # TODO - transforms specify a plugin as an URI (e.g. http://vamp-plugins.org/rdf/plugins/vamp-example-plugins#amplitudefollower)
+        #        we need to find the correspondent plugin name.. It would be nice to have the existing datatype property plugin_name in
+        #        the RDF configuration file of the transform
+        # NOTE - we now use this query to build the n3 file to feed sonic-annotator
+        #        soon the code will be split into vampWT and sonicAnnotatorWT
+        status, results =  self.kp.query(self.jsap.queryUri, self.jsap.getQuery("GET_TRANSFORM_CONSTRUCT", {"transform": transformUri}))
+        getN3FromBindings(results["results"]["bindings"], self.n3File)
                
-        # TODO retrieve input of that actions
-        logging.debug("ActionHandker::handle() -- loading audio file")
+        # load the song
+        # TODO -- for demo purposes we will use default values and a default song
+        logging.debug("ActionHandker::handle() -- loading audio file")    
         data, rate = librosa.load(songName)
+
+        # start the analysis
         logging.debug("ActionHandker::handle() -- performing analysis with " + actionName)
         c = vamp.collect(data, rate, actionName)
-        print(str(c))
         logging.debug("ActionHandker::handle() -- writing results")        
 
-        # sanitize output
+        # write data into SEPA
+        # TODO -- at the moment I put something horrible.. need to fix
         outString = str(c).replace("\n", "").replace("\r", "").replace("'", "").replace('"', '')
-        
-        # TODO -- put data into SEPA
-
         print(self.jsap.getUpdate("ADD_COMPLETION_TIMESTAMP_WITH_OUTPUT", {
             "instance":actionInstance,
             "outputFieldValue": outString,
             "outputFieldName":"output"
         }))
-        
+
+        # add the timestamp to the action
         self.kp.update(self.jsap.updateUri, self.jsap.getUpdate("ADD_COMPLETION_TIMESTAMP_WITH_OUTPUT", {
             "instance":actionInstance,
             "outputFieldValue": outString,
